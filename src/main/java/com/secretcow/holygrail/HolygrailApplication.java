e735c2f3d50959c4fe536b05a97f44929b93e01a
tuckerishere <michael.tucker.a@gmail.com>
2024-12-21 14:03:18 -0600

    initial commit

---
+++ src/main/java/com/secretcow/holygrail/HolygrailApplication.java
@@ -0,0 +1,13 @@
+package com.secretcow.holygrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolygrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolygrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secretcow/holygrail/HolygrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretcow.holygrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolygrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
